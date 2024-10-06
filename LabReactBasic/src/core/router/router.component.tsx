7e544c575b4b8982ae464b2959993b8e99dc8747
Zhery <alejandroramirezescalona34@gmail.com>
2024-10-06 20:08:35 +0200

    refactor: delte unused imports

--- LabReactBasic/src/core/router/router.component.tsx
+++ LabReactBasic/src/core/router/router.component.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { HomeScene } from "../../scenes/Home/Home.scene";
 import { switchRoutes } from "./routes";
 import DetailScene from "../../scenes/Details/Detail.scene";
