5e43b14622c8384fa3422455d8e6f4e5dfeeaa66
Zhery <alejandroramirezescalona34@gmail.com>
2024-12-10 19:50:22 +0100

    initial commit

---
+++ Vue/clase/components/TheNavigation.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="border-b border-b-slate-300 p-3">
+        <NuxtLink to="/">Home</NuxtLink>
+    </nav>
+</template>
+
+
+<script></script>
---
+++ Vue/clase/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <TheNavigation />
+    </header>
+    <main>
+        <slot />
+    </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
